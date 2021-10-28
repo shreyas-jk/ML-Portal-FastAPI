74b9b3f924ffe8a7cdcbbfc227619be3e0b164ae
shreyaskudav <dekudav@gmail.com>
2021-10-29 02:07:38 +0530

    Update

--- prediction.py
+++ prediction.py
@@ -36,7 +36,7 @@ class Prediction():
         self.logger.write_log(self.file_object, 'Preprocessing routine completed successfully')
 
         file_op = FileOperation()
-        kmean_model = file_op.load_model('./Clustering_Model/Kmeans.pkl')
+        kmean_model = file_op.load_model('app/Clustering_Model/Kmeans.pkl')
         clusters = kmean_model.predict(data)
 
         data['clusters'] = clusters
